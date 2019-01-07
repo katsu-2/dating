771f515640466586628820302d394baeb782782f
大平勝也 <k8t2y9.cat@gmail.com>
2019-01-07 12:24:22 +0900

    画像投稿機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :text)
+      params.require(:post).permit(:title, :text, :img)
     end
 end
