6eab16205f4089cb44902eb4f0dd5dbd95ebf062
大平勝也 <k8t2y9.cat@gmail.com>
2019-01-09 13:57:16 +0900

    コメントを追加したが削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_034456) do
+ActiveRecord::Schema.define(version: 2019_01_09_044359) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
