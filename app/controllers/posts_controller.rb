c0cc57e77ab5b4c45064836a6f0888bc3948b526
大平勝也 <k8t2y9.cat@gmail.com>
2019-01-12 10:10:04 +0900

    コメントの削除ボタンの設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to post_path(@post)
     else
       render 'new'
     end
