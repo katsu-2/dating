03d4c04cc09861591bbd33b79547e5c825e63144
大平勝也 <k8t2y9.cat@gmail.com>
2019-01-16 10:26:28 +0900

    ユーザーページにbox-shadowをつけたり

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_044300) do
+ActiveRecord::Schema.define(version: 2019_01_16_001308) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
