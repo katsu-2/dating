5fd14c2ff2d9764c29c29dd472bc5c68c7e62973
大平勝也 <k8t2y9.cat@gmail.com>
2019-01-19 11:39:25 +0900

    コメントのいらないコード削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
       redirect_to post_path(@post)
