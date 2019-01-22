09744e864a1bd59bec45e5fe6bd07a9db119897b
大平勝也 <k8t2y9.cat@gmail.com>
2019-01-22 11:27:54 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   enum gender: { 男: 0, 女: 1 }
+  validates :name, presence: true, length: { maximum: 20 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
 end
